145d03d2a762ad102ef2908496539d2c406f2a66
master18032008 <marcos.ggomes.2020@gmail.com>
2026-06-20 19:42:19 -0300

    Add files via upload

---
+++ 2646460.lua
@@ -0,0 +1,5 @@
+addappid(2646460)
+addappid(3336600)
+addappid(3731990)
+addappid(2646461,0,"b888b7fdf975bfc80d87df6beb2c5d56ada854d2adb6537fce281905a5d74d2a")
+setManifestid(2646461,"2127144436286765740")
---
+++ 3241660.lua
@@ -0,0 +1,7 @@
+-- This lua file has been fetched from the bot VICTOR which is the exclusive property of Piracy Lords server.
+-- Redistribution of VICTORs files is not allowed AT ALL
+-- Join the official here: https://discord.gg/piracylords
+
+addappid(3241660)
+addappid(3241661,0,"0a0fcffb2de6fb0fa6caff076a5a8cb960903d17320968d5c042fd5a4f9ad2ad")
+setManifestid(3241661,"5884486897745351435")
